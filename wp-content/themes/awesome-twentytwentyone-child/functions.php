56eeb5aa075f1d7d48a3c19370694de748c144bb
Matthias Andrasch <777278+programmieraffe@users.noreply.github.com>
2021-03-06 18:19:57 +0100

    add childtheme values

---
+++ wp-content/themes/awesome-twentytwentyone-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+/* enqueue scripts and style from parent theme */
+   
+function twentytwentyone_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+	array( 'twenty-twenty-one-style' ), wp_get_theme()->get('Version') );
+}
+add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
