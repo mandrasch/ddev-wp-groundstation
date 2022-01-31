4c18b9ae8fefd458d87809d46f0cf53336571558
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-01-31 23:42:36 +0100

    clean up repo, please see new version: mandrasch/ddev-pull-wp-scripts

--- wp-content/themes/twentytwentyone-child/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-function child_theme_styles()
-{
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-}
-add_action('wp_enqueue_scripts', 'child_theme_styles');
